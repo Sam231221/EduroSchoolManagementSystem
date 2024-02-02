6ad0dddb108d503b4267dc1c4201e2a719957de6
Sameer Shahi Thakuri (Sam) <70689865+Sam231221@users.noreply.github.com>
2024-02-02 15:12:27 +0545

    more files deleted and views, templates updates

--- MEhub/views.py
+++ MEhub/views.py
@@ -35,17 +35,16 @@ class StudentDeleteView(View):
 class TeacherListView(View):
     def get(self, request):
         context={}
-        return render(request,'1.main/add-teacher.html', context)
+        return render(request,'1.main/teacher/teachers.html', context)
 
 class TeacherEditView(View):
     def get(self, request):
         context={}
-        return render(request,'base.html', context)
-
+        return render(request,'1.main/teacher/edit-teacher.html', context)
 class TeacherAddView(View):
     def get(self, request):
         context={}
-        return render(request,'base.html', context)
+        return render(request,'1.main/teacher/add-teacher.html', context)
 
 class TeacherDeleteView(View):
     def post(self, request):
@@ -60,17 +59,17 @@ class TeacherDeleteView(View):
 class SubjectListView(View):
     def get(self, request):
         context={}
-        return render(request,'base.html', context)
+        return render(request,'1.main/subject/subjects.html', context)
 
 class SubjectEditView(View):
     def get(self, request):
         context={}
-        return render(request,'base.html', context)
+        return render(request,'1.main/subject/edit-subject.html', context)
 
 class SubjectAddView(View):
     def get(self, request):
         context={}
-        return render(request,'base.html', context)
+        return render(request,'1.main/subject/add-subject.html', context)
 
 class SubjectDeleteView(View):
     def post(self, request):
@@ -85,17 +84,20 @@ class SubjectDeleteView(View):
 class InvoiceListView(View):
     def get(self, request):
         context={}
-        return render(request,'base.html', context)
+        return render(request,'1.main/invoice/invoices.html', context)
+
 
 class InvoiceEditView(View):
     def get(self, request):
         context={}
-        return render(request,'base.html', context)
+        return render(request,'1.main/invoice/edit-invoice.html', context)
+
 
 class InvoiceAddView(View):
     def get(self, request):
         context={}
-        return render(request,'base.html', context)
+        return render(request,'1.main/invoice/add-invoice.html', context)
+
 
 class InvoiceDeleteView(View):
     def post(self, request):
